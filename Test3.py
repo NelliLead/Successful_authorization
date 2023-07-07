68e1195251e6a10aac18f534fdd779a06ddbab6f
NelliLead <nellileadovskaia@gmail.com>
2023-07-08 00:05:15 +0300

    Add our test

---
+++ Test3.py
@@ -0,0 +1,40 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.chrome.webdriver import WebDriver
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+
+URL = 'https://www.saucedemo.com/'
+USERNAME = 'standard_user'
+PASSWORD = 'secret_sauce'
+
+def get_driver():
+    driver: WebDriver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+    return driver
+
+def open_page(driver, url):
+    driver.get(url)
+
+def authorization(driver, username, password):
+    element_send_keys(driver, 'user-name', username)
+    element_send_keys(driver, 'password', password)
+    element_click(driver, 'login-button')
+
+def get_element_by_id(driver, locator):
+    return driver.find_element(By.ID, locator)
+
+def element_click(driver, locator):
+    element = get_element_by_id(driver, locator)
+    element.click
+
+def element_send_keys(driver, locator, text):
+    element = get_element_by_id(driver, locator)
+    element.send_keys(text)
+
+
+
+driver = get_driver()
+open_page(driver, URL)
+authorization(driver=driver, username=USERNAME, password=PASSWORD)
+
+driver.quit()
